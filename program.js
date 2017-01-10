522c19c779338d4831ff27b1ce3ddc5959552e2f
Matthew Bush <mbush@hirebotics.com>
2017-01-09 21:27:45 -0600

    Uploading the program file

---
+++ program.js
@@ -0,0 +1,39 @@
+'use strict';
+
+var self = {
+  state: {
+    inputs: {},
+    outputs: {}
+  },
+  recipe: {
+    0: {
+      perform: function perform(callback) {
+        var currState = getRobotState();
+        self.state.inputs = currState.inputs;
+        self.state.outputs = currState.outputs;
+        console.log(self.state);
+        return callback(null);
+      },
+      nextStep: 1
+
+    },
+    1: {
+      nextStep: 2
+    },
+    2: {
+      perform: function perform(callback) {
+        console.log(getStandardDigitalOutput(0));
+        if (getStandardDigitalOutput(0) == '1') {
+          self.recipe[2].nextStep = 0;
+        } else {
+          self.recipe[2].nextStep = 3;
+        }
+        return callback(null);
+      },
+      nextStep: 3
+    },
+    3: {
+      nextStep: 0
+    }
+  }
+};
---
+++ src/program.js
@@ -0,0 +1,37 @@
+let self = {
+    state:{
+      inputs:{},
+      outputs:{},
+    },
+    recipe:{
+      0: {
+        perform: (callback)=>{
+          let currState = getRobotState()
+          self.state.inputs = currState.inputs
+          self.state.outputs = currState.outputs
+          console.log(self.state)
+          return callback(null)
+        },
+        nextStep:1,
+
+      },
+      1: {
+        nextStep:2
+      },
+      2: {
+        perform:(callback)=>{
+          console.log(getStandardDigitalOutput(0))
+          if(getStandardDigitalOutput(0) == '1') {
+              self.recipe[2].nextStep = 0
+            } else {
+              self.recipe[2].nextStep = 3
+            }
+            return callback(null)
+        },
+        nextStep:3
+      },
+      3: {
+        nextStep:0
+      },
+    },
+  }
