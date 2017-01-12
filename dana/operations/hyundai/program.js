5e9e63f76aa250b108a6965e5522a97386888efc
Matthew Bush <mbush@hirebotics.com>
2017-01-11 21:28:33 -0600

    Changing the file around

--- dana/operations/hyundai/program.js
+++ dana/operations/hyundai/program.js
@@ -1,40 +1,41 @@
 'use strict';
 
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-var self = exports.self = {
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+
+var op2 = function perform(state, callback) {
+  var currState = state;
+  self.state.inputs = currState.inputs;
+  self.state.outputs = currState.outputs;
+  console.log(self.state);
+  return callback(null, 3);
+}
+
+var op1 = function perform(state, callback) {
+  var outputs = state.outputs.standardOutputs;
+  if (outputs[0] == '1') {
+    self.recipe[2].nextStep = 0;
+    return callback(null, 0);
+  } else {
+    self.recipe[2].nextStep = 1;
+    return callback(null, 1);
+  }
+}
+
+var self = {
   state: {
     inputs: {},
     outputs: {}
   },
   recipe: {
     0: {
-      perform: function perform(state, callback) {
-        var currState = state;
-        this.state.inputs = currState.inputs;
-        this.state.outputs = currState.outputs;
-        console.log(this.state);
-        console.log('****************************************************')
-        console.log('ROB DOESNT BELIEVE ME!!!!!')
-        return callback(null, 3);
-      },
+      perform: op1,
       nextStep: 3
     },
     1: {
       nextStep: 2
     },
     2: {
-      perform: function perform(state, callback) {
-        var outputs = state.outputs.standardOutputs;
-        if (outputs[0] == '1') {
-          this.recipe[2].nextStep = 0;
-          return callback(null, 0);
-        } else {
-          this.recipe[2].nextStep = 1;
-          return callback(null, 1);
-        }
-      },
+      perform: op2,
       nextStep: 1
     },
     3: {
@@ -42,3 +43,5 @@ var self = exports.self = {
     }
   }
 };
+
+module.exports = _extends({}, self);
